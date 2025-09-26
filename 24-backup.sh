f5147d9d4169b785a044e419f5f0bbd53e91a073
sivakumar reddy <info@joindevops.com>
2025-09-26 08:41:09 +0530

    session-19

--- 24-backup.sh
+++ 24-backup.sh
@@ -7,6 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 SOURCE_DIR=$1
 DEST_DIR=$2
+DAYS=${3:-14} # if not provided considered as 14 days
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
@@ -30,11 +31,19 @@ if [ $# -lt 2 ]; then
 fi
 
 if [ ! -d $SOURCE_DIR ]; then
-    echo -e "$R $SOURCE_DIR does not exist $N"
+    echo -e "$R Source $SOURCE_DIR does not exist $N"
     exit 1
 fi
 
 if [ ! -d $DEST_DIR ]; then
-    echo -e "$R $DEST_DIR does not exist $N"
+    echo -e "$R Destination $DEST_DIR does not exist $N"
     exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+
+if [ ! -z "${FILES}" ]; then
+    echo "Files found"
+else
+    echo "No files to archeive ... $Y SKIPPING $N"
 fi
